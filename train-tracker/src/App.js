e58b60fa5d86c9adda07ba0944769721177f245a
Louise Dupont <louisesdupont@gmail.com>
2024-10-30 16:40:58 -0400

    adding header

--- train-tracker/src/App.js
+++ train-tracker/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import train_icon from './train_icon.png';
 
 function App() {
   return (
     <div className="App">
-      <h1>Train Tracker</h1>
+      <div className='heading'>
+        <img src={train_icon} alt="Train Icon" className="train_icon"/>
+        <h1>Train Tracker</h1>
+      </div>
     </div>
   );
 }
