69bf1ed55d8859600f9bb44a6f73a9a6a2075cbb
molly-pop <157141271+molly-pop@users.noreply.github.com>
2024-10-31 15:49:23 -0400

    Added Map.js file with Leaflet capability. Updated App & index accordingly.

--- train-tracker/src/App.js
+++ train-tracker/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import train_icon from './train_icon.png';
-
 function App() {
+
   return (
+
     <div className="App">
       <div className='heading'>
         <img src={train_icon} alt="Train Icon" className="train_icon"/>
---
+++ train-tracker/src/Map.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix default icon issue
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+});
+
+const Map = () => {
+    return (
+        <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100vh", width: "100%" }}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={[51.505, -0.09]}>
+                <Popup>A popup for this marker!</Popup>
+            </Marker>
+        </MapContainer>
+    );
+};
+
+export default Map;
