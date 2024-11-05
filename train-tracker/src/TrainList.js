c2cfa3bbd0fda2a60705cd1e0af73d070ddbaf4c
tjtemple <tuo70963@temple.edu>
2024-11-04 23:47:33 -0500

    basic css and more information

--- train-tracker/src/TrainList.js
+++ train-tracker/src/TrainList.js
@@ -1,20 +1,35 @@
 import Amtrak from './AmtrakAPI';
 import React from 'react';
+import './TrainList.css';
 
 function TrainList(){
     // simple component just to test stuff
     function MakeTrain({
         train = new Amtrak.Train()
     }){
-        var lastAt = ""
+        var lastAt = "";
+        var dir = "";
+        var status = "";
+        if (train.heading){
+            dir = " (" + train.heading + ")";
+        }
         if (train.stations[train.lastVisitedStation]){
             lastAt = ", last at " + train.stations[train.lastVisitedStation].stationCode;
         }
+        if (train.punctuality){
+            status = " (" + train.punctuality + ")";
+        }
+
         return (
-            <div className = 'train'>
-                <h2>{train.routeName} #{train.number}</h2>
-                <p>{train.from} to {train.to}{lastAt}</p>
+            <div className = 'spacing'>
+                <div className = 'train'>
+                    <h2>#{train.number} {train.routeName}{dir} </h2>
+                    <p>
+                        {train.from} to {train.to}{lastAt}{status}
+                    </p>
+                </div>
             </div>
+            
         )
     } // end MakeTrain
 
@@ -29,11 +44,11 @@ function TrainList(){
         trainData.update();
     },[])
     
-    console.log(trainList);
+    //console.log(trainList);
     
     if (trainList.length != 0){
         return (
-            <div>
+            <div className = 'trainList'>
                 {
                     trainList.map(t =>
                         <MakeTrain train = {t}/>
