53a13f3d8e41d581b281fe04ed6245b517e855b8
molly-pop <157141271+molly-pop@users.noreply.github.com>
2024-11-19 15:54:07 -0500

    Added sidebar styling/collapsible.

--- train-tracker/src/App.js
+++ train-tracker/src/App.js
@@ -21,83 +21,87 @@ function App() {
     const [allTrains, setAllTrains] = useState([]);
     const [allRoutes, setAllRoutes] = useState([]);
     const [allStations, setAllStations] = useState([]);
+    const [sidebarOpen, setSidebarOpen] = useState(false);
 
-    useEffect(() => {
-        api.onUpdated = function() {
-            setAllTrains(this.trains);
-            setAllRoutes(this.routes);
-            setAllStations(this.stations);
-        }
-        api.update();
+    const toggleSidebar = () => {
+        setSidebarOpen(!sidebarOpen);
+    }
 
-    },[]);
+        useEffect(() => {
+            api.onUpdated = function () {
+                setAllTrains(this.trains);
+                setAllRoutes(this.routes);
+                setAllStations(this.stations);
+            }
+            api.update();
 
-    useEffect(() => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((pos)=>{
-                setUserLocation(pos);
-                if (allStations.length > 0 && pos){
-                    setSelectedStation(getClosestStation(allStations, pos).stationCode);
-                }
-            }, 
-            (error) => console.log('error' + error));
-        }
-        
-    }, [allStations]);
-  
-    const HomePage = <Home
-        allTrains={allTrains}
-        allRoutes={allRoutes}
-        allStations={allStations}
-        userLocation={userLocation}
-        selectedStation={selectedStation}
-        setSelectedStation={setSelectedStation}
-        selectedRoute={selectedRoute}
-        setSelectedRoute={setSelectedRoute}
-    />;
+        }, []);
 
-    const MapPage = <TrainMap
-        trains={allTrains}
-        userLocation={userLocation}
-        selectedStation={convertStationCodeToStation(allStations, selectedStation)}
-        selectedRoute={selectedRoute}
-        />;
+        useEffect(() => {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition((pos) => {
+                        setUserLocation(pos);
+                        if (allStations.length > 0 && pos) {
+                            setSelectedStation(getClosestStation(allStations, pos).stationCode);
+                        }
+                    },
+                    (error) => console.log('error' + error));
+            }
 
-  return (
-    <HashRouter>
-        <div className="App">
-          <div className="header">
-                <div className="heading-box">
-                    <img src={train_icon} alt="Train Icon" className="train_icon" />
-                    <h1>TrainTracker</h1>
+        }, [allStations]);
+
+        const HomePage = () => ( <Home
+            allTrains={allTrains}
+            allRoutes={allRoutes}
+            allStations={allStations}
+            userLocation={userLocation}
+            selectedStation={selectedStation}
+            setSelectedStation={setSelectedStation}
+            selectedRoute={selectedRoute}
+            setSelectedRoute={setSelectedRoute}
+        />);
+
+        const MapPage = () => ( <TrainMap
+            trains={allTrains}
+            userLocation={userLocation}
+            selectedStation={convertStationCodeToStation(allStations, selectedStation)}
+            selectedRoute={selectedRoute}
+        />);
+
+        return (
+            <HashRouter>
+                <div className="App">
+                    <div className={`sidebar ${sidebarOpen ? 'open' : ''}`} id="mySidebar">
+                        <img src={train_icon} alt="Train Icon" className="train_icon" />
+                        <button className="openbtn" onClick={toggleSidebar}>
+                            {sidebarOpen ? "Close Sidebar" : "Open Sidebar"}
+                        </button>
+                        {sidebarOpen && (
+                            <ul className="nav">
+                                <li>
+                                    <Link to="/home" onClick={toggleSidebar}>Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/train" onClick={toggleSidebar}>Trains</Link>
+                                </li>
+                                <li>
+                                    <Link to="/map" onClick={toggleSidebar}>Map</Link>
+                                </li>
+                            </ul>
+                        )}
+                    </div>
+
+                    <div className="content" id="main">
+                        <Routes>
+                            <Route path="/" element={<HomePage />} />
+                            <Route path="/home" element={<HomePage />} />
+                            <Route path="/train/:trainInfo" element={<TrainPage allTrains={allTrains} />} />
+                            <Route path="/map" element={<MapPage />} />
+                        </Routes>
+                    </div>
                 </div>
-          </div>
-              
-          <div className='content'>
-              <div className="sidebar">
-                  <ul className="nav">
-                      <li>
-                          <Link to="/home">Home</Link>
-                      </li>
-                      <li>
-                          <Link to="/train">Trains</Link>
-                      </li>
-                      <li>
-                          <Link to="/map">Map</Link>
-                      </li>
-                  </ul>
-              </div>
-
-                <Routes>
-                    <Route path="/" element={HomePage}/>
-                    <Route path="/home" element={HomePage}/>
-                    <Route path="/train/:trainInfo" element={<TrainPage allTrains={allTrains}/>}/>
-                    <Route path="/map" element={MapPage}/>
-                </Routes>
-              </div> 
-          </div>
-    </HashRouter>
-  );
+            </HashRouter>
+        );
 }
 
 export default App;
