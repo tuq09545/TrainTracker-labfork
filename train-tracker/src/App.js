9be3f1ae0e45cf41c5625e653a3bf8aabd60fa68
molly-pop <157141271+molly-pop@users.noreply.github.com>
2024-11-19 20:25:21 -0500

    Massive sidebar style overhaul

--- train-tracker/src/App.js
+++ train-tracker/src/App.js
@@ -7,6 +7,11 @@ import { Routes, Route, HashRouter, Link, useNavigate } from 'react-router-dom';
 import Home from './Home';
 import TrainPage from './TrainPage';
 import TrainMap from './TrainMap';
+import { IoIosArrowDropleft } from "react-icons/io";
+import { IoIosArrowDropright } from "react-icons/io";
+import { IoHomeOutline } from "react-icons/io5";
+import { FaMapLocationDot } from "react-icons/fa6";
+import { IoTrainOutline } from "react-icons/io5";
 
 import {convertStationCodeToStation, getClosestStation} from './functionality/app';
 
@@ -27,71 +32,85 @@ function App() {
         setSidebarOpen(!sidebarOpen);
     }
 
-        useEffect(() => {
-            api.onUpdated = function () {
-                setAllTrains(this.trains);
-                setAllRoutes(this.routes);
-                setAllStations(this.stations);
-            }
-            api.update();
+    useEffect(() => {
+        api.onUpdated = function () {
+            setAllTrains(this.trains);
+            setAllRoutes(this.routes);
+            setAllStations(this.stations);
+        }
+        api.update();
+    }, []);
 
-        }, []);
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((pos) => {
+                    setUserLocation(pos);
+                    if (allStations.length > 0 && pos) {
+                        setSelectedStation(getClosestStation(allStations, pos).stationCode);
+                    }
+                },
+                (error) => console.log('error' + error));
+        }
 
-        useEffect(() => {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition((pos) => {
-                        setUserLocation(pos);
-                        if (allStations.length > 0 && pos) {
-                            setSelectedStation(getClosestStation(allStations, pos).stationCode);
-                        }
-                    },
-                    (error) => console.log('error' + error));
-            }
+    }, [allStations]);
 
-        }, [allStations]);
+    const HomePage = () => ( <Home
+        allTrains={allTrains}
+        allRoutes={allRoutes}
+        allStations={allStations}
+        userLocation={userLocation}
+        selectedStation={selectedStation}
+        setSelectedStation={setSelectedStation}
+        selectedRoute={selectedRoute}
+        setSelectedRoute={setSelectedRoute}
+    />);
 
-        const HomePage = () => ( <Home
-            allTrains={allTrains}
-            allRoutes={allRoutes}
-            allStations={allStations}
-            userLocation={userLocation}
-            selectedStation={selectedStation}
-            setSelectedStation={setSelectedStation}
-            selectedRoute={selectedRoute}
-            setSelectedRoute={setSelectedRoute}
-        />);
-
-        const MapPage = () => ( <TrainMap
-            trains={allTrains}
-            userLocation={userLocation}
-            selectedStation={convertStationCodeToStation(allStations, selectedStation)}
-            selectedRoute={selectedRoute}
-        />);
+    const MapPage = () => ( <TrainMap
+        trains={allTrains}
+        userLocation={userLocation}
+        selectedStation={convertStationCodeToStation(allStations, selectedStation)}
+        selectedRoute={selectedRoute}
+    />);
 
         return (
             <HashRouter>
                 <div className="App">
                     <div className={`sidebar ${sidebarOpen ? 'open' : ''}`} id="mySidebar">
-                        <img src={train_icon} alt="Train Icon" className="train_icon" />
+                        <div className="sidebar-header">
+                            <img src={train_icon} alt="Train Icon" className="train_icon" />
+                            {sidebarOpen && (
+                                <h2 className="sidebar-heading">TrainTracker</h2>
+                            )}
+                        </div>
                         <button className="openbtn" onClick={toggleSidebar}>
-                            {sidebarOpen ? "Close Sidebar" : "Open Sidebar"}
+                            {sidebarOpen ? (
+                                <IoIosArrowDropleft size={30} />
+                            ) : (
+                                <IoIosArrowDropright size={30} />
+                            )}
                         </button>
-                        {sidebarOpen && (
-                            <ul className="nav">
-                                <li>
-                                    <Link to="/home" onClick={toggleSidebar}>Home</Link>
-                                </li>
-                                <li>
-                                    <Link to="/train" onClick={toggleSidebar}>Trains</Link>
-                                </li>
-                                <li>
-                                    <Link to="/map" onClick={toggleSidebar}>Map</Link>
-                                </li>
-                            </ul>
-                        )}
+                        {sidebarOpen ? (
+                            <div className="nav">
+                                <Link to="/home" className="nav-link">Home</Link>
+                                <Link to="/train" className="nav-link">Trains</Link>
+                                <Link to="/map" className="nav-link">Map</Link>
+                            </div>
+                        ) : (
+                            <div className="nav-icons">
+                                <Link to="/home" className="nav-icon-link">
+                                    <IoHomeOutline size={30} />
+                                </Link>
+                                <Link to="/train" className="nav-icon-link">
+                                    <IoTrainOutline size={30} />
+                                </Link>
+                                <Link to="/map" className="nav-icon-link">
+                                    <FaMapLocationDot size={30} />
+                                </Link>
+                            </div>
+                            )}
                     </div>
 
-                    <div className={`content ${!sidebarOpen ? 'sidebar-closed' : ''}`} id="main">
+                    <div className={`content ${!sidebarOpen ? 'sidebar-closed' : 'sidebar-open'}`} id="main">
                         <Routes>
                             <Route path="/" element={<HomePage />} />
                             <Route path="/home" element={<HomePage />} />
